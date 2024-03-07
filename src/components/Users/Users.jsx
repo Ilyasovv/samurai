60ad5bf178d01fdded00cdca0d474142f01d08e1
MisterAbai <abaiilyasovv@gmail.com>
2024-03-07 20:13:58 +0500

    g

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/user.jpg";
 let Users = (props) => {
   if (props.users.length === 0) {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
-        props.setUsers();
+        props.setUsers(response.data.items);
       });
   }
 
@@ -15,9 +17,13 @@ let Users = (props) => {
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={styles.userPhoto} />
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                className={styles.userPhoto}
+              />
             </div>
             <div>
+              ss
               {u.followed ? (
                 <button
                   onClick={() => {
@@ -39,12 +45,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
