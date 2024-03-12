c6a138f6fc4823c8dc54cb3b64dfc3117a72a5e9
MisterAbai <abaiilyasovv@gmail.com>
2024-03-12 19:05:34 +0500

    g

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,6 +4,9 @@ const SET_USERS = "SET_USERS";
 
 let initialState = {
   users: [],
+  pageSize: 5,
+  totalUsersCount: 21,
+  currentPage: 3,
 };
 
 const usersReducer = (state = initialState, action) => {
